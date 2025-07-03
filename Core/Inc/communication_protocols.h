71c0bc32410acfd186898db084dd3944923b306f
Carlos Cerqueira <ce.cesc01@gmail.com>
2025-07-03 17:06:08 -0300

    Initial commit

---
+++ Core/Inc/communication_protocols.h
@@ -0,0 +1,15 @@
+/*
+ * iic.h
+ *
+ *  Created on: Jul 1, 2025
+ *      Author: carlos
+ */
+
+#ifndef INC_COMMUNICATION_PROTOCOLS_H_
+#define INC_COMMUNICATION_PROTOCOLS_H_
+
+void vI2C_Management_Task(void *pvParameters);
+
+void vUART_Management_Task(void *pvParameters);
+
+#endif /* INC_COMMUNICATION_PROTOCOLS_H_ */
